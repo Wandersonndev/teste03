e01fd62f9f2655fb5029578b71633648458266e3
crafto <107423898+Wandersonndev@users.noreply.github.com>
2023-08-17 14:21:59 -0300

    adicionando o css

--- js/foto64.js
+++ js/foto64.js
@@ -14,10 +14,89 @@ function converterImagem(){
        var fotoP = document.getElementById("fotoP").value = imagemBase64;
       console.log("URL:",fotoP)
 
-      var  img = document.createElement("img")
+      var  img = document.getElementById("imgPrincipal")
       img.src = imagemBase64;
-      img.id = "imgP"
-      document.getElementById("imgPrincipal").innerHTML = img.outerHTML
+      
+
+     }
+     //ler o conteudo
+     lerArquivo.readAsDataURL(carregarImagem);
+  }
+}
+//Imagem 1
+function converterImagem1(){
+  var receberArquivo = document.getElementById("foto1").files;
+  //console.log(receberArquivo)
+  // verificar se a imagem foi pegada
+  if(receberArquivo.length > 0){
+    //Carregar img
+    var carregarImagem = receberArquivo[0];
+    //console.log(carregarImagem);
+    //permitir ler o conteudo do dispositivo
+     var lerArquivo = new FileReader();
+  
+     lerArquivo.onload = function(arquivoCarregado){
+      var imagemBase64 = arquivoCarregado.target.result;
+       var fotoP = document.getElementById("img11").value = imagemBase64;
+      console.log("URL:",fotoP)
+
+      var  img = document.getElementById("img1")
+      img.src = imagemBase64;
+      
+
+     }
+     //ler o conteudo
+     lerArquivo.readAsDataURL(carregarImagem);
+  }
+}
+//imgagem 2
+
+function converterImagem2(){
+  var receberArquivo = document.getElementById("foto2").files;
+  //console.log(receberArquivo)
+  // verificar se a imagem foi pegada
+  if(receberArquivo.length > 0){
+    //Carregar img
+    var carregarImagem = receberArquivo[0];
+    //console.log(carregarImagem);
+    //permitir ler o conteudo do dispositivo
+     var lerArquivo = new FileReader();
+  
+     lerArquivo.onload = function(arquivoCarregado){
+      var imagemBase64 = arquivoCarregado.target.result;
+       var fotoP = document.getElementById("img22").value = imagemBase64;
+      console.log("URL:",fotoP)
+
+      var  img = document.getElementById("img2")
+      img.src = imagemBase64;
+      
+
+     }
+     //ler o conteudo
+     lerArquivo.readAsDataURL(carregarImagem);
+  }
+}
+
+//imgagem 3
+function converterImagem3(){
+  var receberArquivo = document.getElementById("foto3").files;
+  //console.log(receberArquivo)
+  // verificar se a imagem foi pegada
+  if(receberArquivo.length > 0){
+    //Carregar img
+    var carregarImagem = receberArquivo[0];
+    //console.log(carregarImagem);
+    //permitir ler o conteudo do dispositivo
+     var lerArquivo = new FileReader();
+  
+     lerArquivo.onload = function(arquivoCarregado){
+      var imagemBase64 = arquivoCarregado.target.result;
+       var fotoP = document.getElementById("img33").value = imagemBase64;
+      console.log("URL:",fotoP)
+
+      var  img = document.getElementById("img3")
+      img.src = imagemBase64;
+      
 
      }
      //ler o conteudo
