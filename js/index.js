fc6e3ed5096a3f577af348dc76da93dfe00f5fab
crafto <107423898+Wandersonndev@users.noreply.github.com>
2023-09-24 11:36:26 -0300

    teste d 24/09

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const db = getFirestore(app);
 
 const produtos = document.getElementById("produtos");
 const produtosSearch = document.getElementById("produtosSearch")
-let produtosExibidos = 4 ;
+let produtosExibidos = 4;
 
 
 // Função para exibir as imagens dos produtos do Firestore
@@ -24,68 +24,69 @@ async function exibirImagens() {
   try {
     const produtosRef = collection(db, "produtos");
     const produtosSnapshot = await getDocs(produtosRef);
-     produtos.innerHTML="";
-     let produtosExibidosAtual = "";
+    produtos.innerHTML = "";
+    let produtosExibidosAtual = "";
 
     produtosSnapshot.forEach((doc) => {
-      if (produtosExibidosAtual < produtosExibidos){
-      const data = doc.data();
-      const imgUrl = data.imgP;
-      const name = data.nome; 
-      const preco = data.precos
-      const docId = doc.id;
+      if (produtosExibidosAtual < produtosExibidos) {
+        const data = doc.data();
+        const imgUrl = data.imgP;
+        const name = data.nome;
+        const preco = data.precos
+        const docId = doc.id;
 
-      // Criar um elemento de imagem e atribuir a URL como a origem (src) da imagem
-      const imgElement = document.createElement("img");
-      imgElement.src = imgUrl;
-      imgElement.classList.add("img");
-      const fundo1 = document.createElement("div");
-      fundo1.classList.add("fundo1");   
-      const titulo = document.createElement("h1");
-      titulo.classList.add("nome")
-      titulo.innerHTML= name;
-      const precos = document.createElement("span");
-      precos.classList.add("precos")
-      precos.innerHTML=`R$${preco}`
-      const vermais = document.createElement("button")
-      vermais.classList.add("vermais")
-      vermais.innerHTML ="VIEW MORE"
-      vermais.addEventListener("click", function () {
-        const url = `produto.html?docId=${encodeURIComponent(docId)}&name=${encodeURIComponent(name)}&preco=${encodeURIComponent(preco)}`;
-        window.location.href = url;
+        // Criar um elemento de imagem e atribuir a URL como a origem (src) da imagem
+        const imgElement = document.createElement("img");
+        imgElement.src = imgUrl;
+        imgElement.classList.add("img");
+        const fundo1 = document.createElement("div");
+        fundo1.classList.add("fundo1");
+        const titulo = document.createElement("h1");
+        titulo.classList.add("nome")
+        titulo.innerHTML = name;
+        const precos = document.createElement("span");
+        precos.classList.add("precos")
+        precos.innerHTML = `R$${preco}`
+        const vermais = document.createElement("button")
+        vermais.classList.add("vermais")
+        vermais.innerHTML = "VIEW MORE"
+        vermais.addEventListener("click", function () {
+          const url = `produto.html?docId=${encodeURIComponent(docId)}&name=${encodeURIComponent(name)}&preco=${encodeURIComponent(preco)}`;
+          window.location.href = url;
+        });
+        const fundo2 = document.createElement("div")
+        fundo2.classList.add("fundo2")
+        const divimg = document.createElement("div")
+        divimg.classList.add("divimg")
+        const li = document.createElement("li");
+        li.id = "lii"
+
+
+
+        divimg.appendChild(imgElement);
+        fundo1.appendChild(divimg)
+        fundo1.appendChild(precos)
+        fundo1.appendChild(fundo2)
+        fundo2.appendChild(titulo)
+        fundo2.appendChild(vermais)
+        li.appendChild(fundo1);
+        produtos.appendChild(li);
+        produtosExibidosAtual++;
+        document.getElementById("spinner").classList.remove("spinner");
+      }
+    });
+    // Verifique se ainda há mais produtos para carregar
+    if (produtosExibidosAtual < produtosSnapshot.size) {
+      const loadMoreButton = document.getElementById("loadMoreButton")
+      loadMoreButton.addEventListener("click", () => {
+        // Incrementa o número de produtos exibidos quando o botão é clicado
+        produtosExibidos += 4;
+        exibirImagens();
       });
-      const fundo2 =document.createElement("div")
-      fundo2.classList.add("fundo2")
-      const divimg = document.createElement("div")
-      divimg.classList.add("divimg")
-      const li = document.createElement("li");
-      li.id="lii"
-     
-
-  
-      divimg.appendChild(imgElement);
-      fundo1.appendChild(divimg) 
-      fundo1.appendChild(precos)
-      fundo1.appendChild(fundo2)
-      fundo2.appendChild(titulo)
-      fundo2.appendChild(vermais)
-      li.appendChild(fundo1);
-      produtos.appendChild(li);
-      produtosExibidosAtual++;
     }
-    });
- // Verifique se ainda há mais produtos para carregar
-  if (produtosExibidosAtual < produtosSnapshot.size) {
-    const loadMoreButton = document.getElementById("loadMoreButton")
-    loadMoreButton.addEventListener("click", () => {
-      // Incrementa o número de produtos exibidos quando o botão é clicado
-      produtosExibidos += 4;
-      exibirImagens();
-    });
+  } catch (error) {
+    console.error("Erro ao obter dados do Firestore:", error);
   }
-} catch (error) {
-  console.error("Erro ao obter dados do Firestore:", error);
-}
 }
 
 
@@ -122,7 +123,7 @@ async function exibirImagensSearch() {
       vermais.classList.add("vermaisSearch");
       vermais.innerHTML = "VIEW MORE";
       vermais.addEventListener("click", function () {
-        const url = `produto.html?docId=${encodeURIComponent(docId)}&name=${encodeURIComponent(name)}&preco=${encodeURIComponent(preco)}`;
+        const url = `produto.html?docId=${encodeURIComponent(docId)}`;
         window.location.href = url;
       });
       const fundo2 = document.createElement("div");
@@ -148,7 +149,7 @@ async function exibirImagensSearch() {
 
 
 // Chamar a função para exibir as imagens quando a página carregar
-window.addEventListener("load",() =>{
+window.addEventListener("load", () => {
   exibirImagens();
   exibirImagensSearch();
 });
--- js/produtos.js
+++ js/produtos.js
@@ -1,17 +1,63 @@
-let productData = {
-  imgUrl: "",
-  name: "",
-  preco: ""
+// produto.html
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCpCFl38gfVWmCQHarvtXECnTRc5TfFtA",
+  authDomain: "catalogo-355b9.firebaseapp.com",
+  projectId: "catalogo-355b9",
+  storageBucket: "catalogo-355b9.appspot.com",
+  messagingSenderId: "76937050583",
+  appId: "1:76937050583:web:0b287f931eea3144e2b392"
 };
 
-// ...
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-vermais.addEventListener("click", function () {
-  // Armazene os dados do produto nas variáveis globais
-  productData.imgUrl = imgUrl;
-  productData.name = name1;
-  productData.preco = preco;
+// Função para obter os detalhes do produto com base no ID
+async function obterDetalhesDoProduto() {
+  try {
+    // Obtenha o ID do produto dos parâmetros da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const docId = urlParams.get("docId");
 
-  // Redirecione para a próxima página (produto.html)
-  window.location.href = "produto.html";
+    // Consulte o Firestore para obter os detalhes do produto com base no ID
+    const produtoRef = doc(db, "produtos", docId);
+    const produtoSnapshot = await getDoc(produtoRef);
+
+    if (produtoSnapshot.exists()) {
+      // Produto encontrado, recupere os dados
+      const data = produtoSnapshot.data();
+      const imgUrl = data.imgP;
+      const ImgUrl1 = data.img1;
+      const ImgUrl2 = data.img2;
+      const ImgUrl3 = data.img3
+      const name = data.nome;
+      const preco = data.precos;
+
+      const imagenCatalogo0 = document.getElementById("imagenCatalogo0")
+      const imagenCatalogo1 = document.getElementById("imagenCatalogo1")
+      const imagenCatalogo2 = document.getElementById("imagenCatalogo2")
+      const imagenCatalogo3 = document.getElementById("imagenCatalogo3")
+      const name1 = document.getElementById("name");
+      const preco1 = document.getElementById("preco");
+      imagenCatalogo0.src = imgUrl;
+      imagenCatalogo1.src = ImgUrl1;
+      imagenCatalogo2.src = ImgUrl2;
+      imagenCatalogo3.src = ImgUrl3;
+      name1.innerHTML = name;
+      preco1.innerHTML = `R$ ${preco}`;
+      document.getElementById("spinner").classList.remove("spinner");
+    } else {
+      console.log("Produto não encontrado");
+    }
+  } catch (error) {
+    console.error("Erro ao obter detalhes do produto:", error);
+  }
+}
+
+// Chame a função para obter os detalhes do produto quando a página carregar
+window.addEventListener("load", () => {
+  obterDetalhesDoProduto();
 });
